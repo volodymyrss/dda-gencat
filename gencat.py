79afbb8af8d1b21a8b13150c79102dbee8498677
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2017-08-31 11:33:28 +0200

    deeper tests

--- gencat.py
+++ gencat.py
@@ -17,6 +17,8 @@ class GenCat(ddosa.DataAnalysis):
 
     cached=True
 
+    cat_attribute="cat"
+
     def main(self):
         catfn="generated_cat.fits"
 
@@ -39,8 +41,29 @@ class GenCat(ddosa.DataAnalysis):
         cat[self.output_structure].data=nd
         cat.writeto(catfn,clobber=True)
 
-        self.cat = ddosa.DataFile(catfn)
+        setattr(self,self.cat_attribute,ddosa.DataFile(catfn))
 
+class GRcat(GenCat):
+    output_structure = "GNRL-REFR-CAT"
+
+    cat_attribute="_cat"
+
+    def map_entry_to_fits_record(self,cat_entry,fits_record):
+        fits_record['RA_OBJ']=cat_entry['RA']
+        fits_record['DEC_OBJ'] = cat_entry['DEC']
+        fits_record['NAME'] = cat_entry['NAME']
+        fits_record['SOURCE_ID'] = cat_entry['NAME']
+        fits_record['ISGRI_FLAG'] = cat_entry.get('ISGRI_FLAG',1)
+        fits_record['ISGRI_FLAG2'] = cat_entry.get('ISGRI_FLAG', 0)
+        fits_record['ISGR_FLUX_1'] = cat_entry.get('ISGRI_FLUX_1', 1000)
+        fits_record['ISGR_FLUX_2'] = cat_entry.get('ISGRI_FLUX_2', 1000)
+
+    @property
+    def cat(self):
+        return self._cat.get_path()
+
+class ISGRIRefCat(GRcat):
+    pass
 
 class CatForImage(GenCat):
     output_structure = "ISGR-SRCL-CAT"
@@ -48,5 +71,16 @@ class CatForImage(GenCat):
     def map_entry_to_fits_record(self,cat_entry,fits_record):
         fits_record['RA_OBJ']=cat_entry['RA']
         fits_record['DEC_OBJ'] = cat_entry['DEC']
+        fits_record['NAME'] = cat_entry['NAME']
+        fits_record['SOURCE_ID'] = cat_entry['NAME']
+        fits_record['ISGRI_FLAG'] = cat_entry.get('ISGRI_FLAG',1)
+
+
+class CatForSpectra(GenCat):
+    output_structure = "ISGR-SRCL-RES"
+
+    def map_entry_to_fits_record(self, cat_entry, fits_record):
+        fits_record['RA_FIN'] = cat_entry['RA']
+        fits_record['DEC_FIN'] = cat_entry['DEC']
         fits_record['NAME'] = cat_entry['NAME']
         fits_record['SOURCE_ID'] = cat_entry['NAME']
--- tests/test_gencat.py
+++ tests/test_gencat.py
@@ -1,4 +1,5 @@
 import sys
+import os
 sys.path.append("/home/savchenk/work/dda/dda-gencat")
 import astropy.io.fits as pyfits
 
@@ -19,6 +20,124 @@ def test_imcat():
     gcat=gencat.CatForImage().get()
 
     d=pyfits.open(gcat.cat.get_path())[1].data
+
+    assert len(d)==1
+    assert d[0]['NAME']==cat.catalog[0]['NAME']
+    assert d[0]['RA_OBJ']==cat.catalog[0]['RA']
+    assert d[0]['DEC_OBJ'] == cat.catalog[0]['DEC']
+
+
+def test_grcat():
+    import ddosa
+    import gencat
+
+    cat=gencat.SourceCatalog()
+    cat.catalog=[
+        dict(
+                NAME="TEST_SOURCE",
+                RA=83,
+                DEC=22,
+             )
+    ]
+    cat.promote()
+
+    gcat=gencat.GRcat(use_read_caches=[]).get()
+
+    d=pyfits.open(gcat.cat)[1].data
+
+    assert len(d)==1
+    assert d[0]['NAME']==cat.catalog[0]['NAME']
+    assert d[0]['RA_OBJ']==cat.catalog[0]['RA']
+    assert d[0]['DEC_OBJ'] == cat.catalog[0]['DEC']
+
+
+def test_igrcat():
+    import ddosa
+    import gencat
+
+    cat=gencat.SourceCatalog()
+    cat.catalog=[
+        dict(
+                NAME="TEST_SOURCE",
+                RA=83,
+                DEC=22,
+             )
+    ]
+    cat.promote()
+
+    gcat=gencat.ISGRIRefCat(use_read_caches=[]).get()
+
+    d=pyfits.open(gcat.cat)[1].data
+
+    assert len(d)==1
+    assert d[0]['NAME']==cat.catalog[0]['NAME']
+    assert d[0]['RA_OBJ']==cat.catalog[0]['RA']
+    assert d[0]['DEC_OBJ'] == cat.catalog[0]['DEC']
+
+
+def test_catforspec():
+    import ddosa
+    import gencat
+
+    cat=gencat.SourceCatalog()
+    cat.catalog=[
+        dict(
+                NAME="TEST_SOURCE",
+                RA=83,
+                DEC=22,
+             )
+    ]
+    cat.promote()
+
+    gcat=gencat.CatForSpectra().get()
+
+    d=pyfits.open(gcat.cat.get_path())[1].data
+
+    assert len(d)==1
+    assert d[0]['NAME']==cat.catalog[0]['NAME']
+    assert d[0]['RA_FIN']==cat.catalog[0]['RA']
+    assert d[0]['DEC_FIN'] == cat.catalog[0]['DEC']
+
+def test_catextract_grcat():
+    import ddosa
+    import gencat
+
+    cat=gencat.SourceCatalog()
+    cat.catalog=[
+        dict(
+                NAME="TEST_SOURCE",
+                RA=83,
+                DEC=22,
+             )
+    ]
+    cat.promote()
+
+    scw=ddosa.ScWData(input_scwid="066500220010.001")
+    scw.get().promote()
+
+    igrcat=gencat.ISGRIRefCat(use_read_caches=[]).get()
+    assert os.path.exists(igrcat.cat)
+    d = pyfits.open(igrcat.cat)[1].data
+    assert len(d) == 1
+    assert d[0]['NAME'] == cat.catalog[0]['NAME']
+    assert d[0]['RA_OBJ'] == cat.catalog[0]['RA']
+    assert d[0]['DEC_OBJ'] == cat.catalog[0]['DEC']
+    assert d[0]['ISGRI_FLAG'] == 1
+    assert d[0]['ISGRI_FLAG2'] == 0
+
+    ecat=ddosa.CatExtract(use_read_caches=[]).get()
+    assert isinstance(ecat.input_cat,gencat.ISGRIRefCat)
+
+    assert os.path.exists(ecat.input_cat.cat)
+    d=pyfits.open(ecat.input_cat.cat)[1].data
+    assert len(d) == 1
+    assert d[0]['NAME'] == cat.catalog[0]['NAME']
+    assert d[0]['RA_OBJ'] == cat.catalog[0]['RA']
+    assert d[0]['DEC_OBJ'] == cat.catalog[0]['DEC']
+    assert d[0]['ISGRI_FLAG'] == 1
+    assert d[0]['ISGRI_FLAG2'] == 0
+
+    d=pyfits.open(ecat.cat.get_path())[1].data
 
     assert len(d)==1
     assert d[0]['NAME']==cat.catalog[0]['NAME']
